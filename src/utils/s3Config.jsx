acb7690b5ea9b969f677e475f7ca70ee3749feac
Rajnish Kumar Singh <rajnish31july@gmail.com>
2024-12-22 11:05:17 +0530

    ap-south-1

--- src/utils/s3Config.jsx
+++ src/utils/s3Config.jsx
@@ -2,7 +2,7 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
 const s3Client = new S3Client({
-  region: import.meta.env.VITE_AWS_REGION,
+  region: import.meta.env.VITE_AWS_REGION || 'ap-south-1',
   credentials: {
     accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
     secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,

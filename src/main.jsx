4cd6c2bba6512c4a70416678bfa000434a408573
Rajnish Kumar Singh <rajnish31july@gmail.com>
2024-12-30 21:38:01 +0530

    added media queries

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
-// import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
